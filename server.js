de34259e821955777c3a5e2cff90ce8699d69032
Theohr <47179221+Theohr@users.noreply.github.com>
2020-01-09 13:07:39 +0000

    COnnected to DB

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require("express");
+var mongoose = require("mongoose");
 
 var app = express();
 var port = 9000;
 
+// Database Connection String
+var uri = "mongodb+srv://theo123:theo1234@ovp-jowv1.mongodb.net/test?retryWrites=true&w=majority";
+
+
 // Connects to the localhost:9000 and enstablishing a database connection
 var server = app.listen(port, function() {
-
+    // Connect to Mongoose.
+    mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}).then((test) => {
+        console.log("Connected to DB");
+    });
+    
     // Some output for the interested reader...
     console.log("Listening on " + port);
 })
